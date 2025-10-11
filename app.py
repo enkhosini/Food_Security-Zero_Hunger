40e04ac77c220c3d21b0272918b7359e619a4f78
Sobonga <smdlovu1@gmail.com>
2025-10-11 21:42:29 +0200

    added all the nesseccary pages

--- app.py
+++ app.py
@@ -7,9 +7,34 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
+
+@app.route("/pre_dashboard", methods=['POST', 'GET'])
+def pre_dashboard():
+    return render_template('pre_dashboard.html')
+
+
+@app.route("/post_dashboard", methods=['POST', 'GET'])
+def post_dashboard():
+    return render_template('post_dashboard.html')
+
+
+@app.route("/farmers_market", methods=['POST', 'GET'])
+def farmers_market():
+    return render_template('farmers_market.html')
+
+
+@app.route("/insurance_page", methods=['POST', 'GET'])
+def home():
+    return render_template('insurance_page.html')
+
+
+@app.route("/vision", methods=['POST', 'GET'])
+def vision():
+    return render_template('vision.html')
+
+@app.route("/account", methods=['POST', 'GET'])
+def account():
+    return render_template('account.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
