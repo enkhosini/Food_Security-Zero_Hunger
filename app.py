e334e7da8e7a94d0a5ea63947e1eee1a8b9d34ce
Sobonga <smdlovu1@gmail.com>
2025-11-17 00:10:18 +0200

    amazing

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 
-
-
-dataframe = pd.read_csv()
+# dataframe = pd.read_csv()
 
 app = Flask(__name__)
 
